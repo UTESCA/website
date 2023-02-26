c6e905d498bf6d491245fc96f5653699fdac57dd
Danny Lu <danny.lu075@gmail.com>
2023-02-26 13:42:09 -0500

    Added every member and their pfp

--- pages/team.tsx
+++ pages/team.tsx
@@ -5,51 +5,19 @@ import MemberCard from "../components/cards/MemberCard";
 import { google } from "googleapis";
 const Nav = dynamic(() => import("../components/nav/Nav"));
 const Footer = dynamic(() => import("../components/footer/Footer"));
-
+import {
+  corporateRelations,
+  finance,
+  internalEnagements,
+  mediaMarketing,
+  presidents,
+  webTeam,
+} from "../data/MemberInfo";
 // const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
 // const auth = new google.auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET);
 // // const DynamicComponent = dynamic(() =>
 // //   import("../components/hello").then((mod) => mod.Hello)
 // // );
-
-const members = [
-  {
-    name: "Danny Lu",
-    position: "Web Developer",
-    image:
-      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260",
-  },
-  {
-    name: "Xiao Zhang",
-    position: "Web Developer",
-    image:
-      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260",
-  },
-  {
-    name: "Bobby Li",
-    position: "CEO",
-    image:
-      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260",
-  },
-  {
-    name: "Bobby Li",
-    position: "CEO",
-    image:
-      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260",
-  },
-  {
-    name: "Bobby Li",
-    position: "CEO",
-    image:
-      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260",
-  },
-  {
-    name: "Bobby Li",
-    position: "CEO",
-    image:
-      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260",
-  },
-];
 
 const Team: NextPage = () => {
   return (
@@ -63,7 +31,7 @@ const Team: NextPage = () => {
           data-aos="fade-up-right"
         >
           <h1 className="font-bold leading-tight text-2xl mt-0 mb-2">
-            Exec Team
+            Presidents
             <hr className="h-px mt-2 bg-gray-200 border-0 dark:bg-gray-700" />
           </h1>
         </div>
@@ -72,7 +40,7 @@ const Team: NextPage = () => {
           className="grid gap-10 mx-auto py-10 sm:grid-cols-2 lg:grid-cols-4 lg:max-w-screen-lg"
           data-aos="fade-up-right"
         >
-          {members.map((member, idx) => (
+          {presidents.map((member, idx) => (
             <MemberCard
               key={idx}
               name={member.name}
@@ -86,7 +54,7 @@ const Team: NextPage = () => {
           data-aos="fade-up-right"
         >
           <h1 className="font-bold leading-tight text-2xl text-rose mt-0 mb-2">
-            Marketing
+            Media & Marketing
             <hr className="h-px mt-2 bg-gray-200 border-0 dark:bg-gray-700" />
           </h1>
         </div>
@@ -94,7 +62,7 @@ const Team: NextPage = () => {
           className="grid gap-10 mx-auto py-10 sm:grid-cols-2 lg:grid-cols-4 lg:max-w-screen-lg"
           data-aos="fade-up-right"
         >
-          {members.map((member, idx) => (
+          {mediaMarketing.map((member, idx) => (
             <MemberCard
               key={idx}
               name={member.name}
@@ -108,7 +76,7 @@ const Team: NextPage = () => {
           data-aos="fade-up-right"
         >
           <h1 className="font-bold leading-tight text-2xl mt-0 mb-2">
-            Web Design
+            Internal Engagements
             <hr className="h-px mt-2 bg-gray-200 border-0 dark:bg-gray-700" />
           </h1>
         </div>
@@ -116,7 +84,7 @@ const Team: NextPage = () => {
           className="grid gap-10 mx-auto py-10 sm:grid-cols-2 lg:grid-cols-4 lg:max-w-screen-lg"
           data-aos="fade-up-right"
         >
-          {members.map((member, idx) => (
+          {internalEnagements.map((member, idx) => (
             <MemberCard
               key={idx}
               name={member.name}
@@ -126,6 +94,76 @@ const Team: NextPage = () => {
           ))}
         </div>
       </div>
+
+      <div
+        className="grid gap-5 mx-auto sm:grid-cols-2 lg:grid-cols-5 lg:max-w-screen-lg"
+        data-aos="fade-up-right"
+      >
+        <h1 className="font-bold leading-tight text-2xl mt-0 mb-2">
+          Corporate Relations
+          <hr className="h-px mt-2 bg-gray-200 border-0 dark:bg-gray-700" />
+        </h1>
+      </div>
+      <div
+        className="grid gap-10 mx-auto py-10 sm:grid-cols-2 lg:grid-cols-4 lg:max-w-screen-lg"
+        data-aos="fade-up-right"
+      >
+        {corporateRelations.map((member, idx) => (
+          <MemberCard
+            key={idx}
+            name={member.name}
+            position={member.position}
+            image={member.image}
+          />
+        ))}
+      </div>
+
+      <div
+        className="grid gap-5 mx-auto sm:grid-cols-2 lg:grid-cols-5 lg:max-w-screen-lg"
+        data-aos="fade-up-right"
+      >
+        <h1 className="font-bold leading-tight text-2xl mt-0 mb-2">
+          Finance
+          <hr className="h-px mt-2 bg-gray-200 border-0 dark:bg-gray-700" />
+        </h1>
+      </div>
+      <div
+        className="grid gap-10 mx-auto py-10 sm:grid-cols-2 lg:grid-cols-4 lg:max-w-screen-lg"
+        data-aos="fade-up-right"
+      >
+        {finance.map((member, idx) => (
+          <MemberCard
+            key={idx}
+            name={member.name}
+            position={member.position}
+            image={member.image}
+          />
+        ))}
+      </div>
+
+      <div
+        className="grid gap-5 mx-auto sm:grid-cols-2 lg:grid-cols-5 lg:max-w-screen-lg"
+        data-aos="fade-up-right"
+      >
+        <h1 className="font-bold leading-tight text-2xl mt-0 mb-2">
+          Web Team
+          <hr className="h-px mt-2 bg-gray-200 border-0 dark:bg-gray-700" />
+        </h1>
+      </div>
+      <div
+        className="grid gap-10 mx-auto py-10 sm:grid-cols-2 lg:grid-cols-4 lg:max-w-screen-lg"
+        data-aos="fade-up-right"
+      >
+        {webTeam.map((member, idx) => (
+          <MemberCard
+            key={idx}
+            name={member.name}
+            position={member.position}
+            image={member.image}
+          />
+        ))}
+      </div>
+
       <Suspense fallback={<div>Loading...</div>}>
         <Footer />
       </Suspense>
